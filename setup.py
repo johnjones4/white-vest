fdee4ad28979ff771a57b4ae4b3739adfe264c8b
John Jones <johnjones4@gmail.com>
2021-03-07 15:43:37 -0500

    package fix

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ with open("Readme.md", "r") as fh:
 
 setuptools.setup(
     name="whitevest-johnjones4",
-    version="0.0.6",
+    version="0.0.7",
     author="John Jones",
     author_email="john@johnjones.family",
     description="White Vest is a project for collecting, logging, emitting, and visualizing telemetry from a model rocket containing an inboard Raspberry Pi Zero with another Raspberry Pi receiving telemetry.",
